2fdd5e839fb91ff1c48ac038fc80b7c202eae8ce
fernandoscjunior <fernandojrsud@icloud.com>
2026-03-18 21:21:09 -0300

    Fixing problem I introduced silly-ah

--- app/lib/action.ts
+++ app/lib/action.ts
@@ -110,19 +110,9 @@ export async function createInvoice(prevState: State, formData: FormData) {
 }
 
 export async function deleteInvoice(id: string) {
-  try {
-    await sql`DELETE FROM invoices WHERE id = ${id}`;
-  } catch (error) {
-    console.error(error);
-    return {
-      message: "Database error: Failed to Delete Invoice",
-    };
-  }
-  
+  await sql`DELETE FROM invoices WHERE id = ${id}`;
   revalidatePath("/dashboard/invoices");
 }
-
-//Remove try catch block from deleteInvoice
 
 export async function authenticate(
   prevState: string | undefined,
